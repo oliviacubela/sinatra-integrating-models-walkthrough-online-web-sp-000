a342012234da4a1bcf004d962e968cc2a8a15692
Olivia Cubela <oliviacubela@gmail.com>
2020-09-22 09:16:29 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,1 +1,11 @@
-# Your TextAnalyzer model code will go here.
+class TextAnalyzer
+  attr_reader :text 
+
+  def initialize(text)
+    @text = text.downcase
+  end
+
+  def count_of_words 
+    words = text.split(" ")
+    words.count 
+  end
